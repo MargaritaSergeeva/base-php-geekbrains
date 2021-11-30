378bad93266350285e5dd0c6060ad2195cdfcefe
Margarita <margaritk@inbox.ru>
2021-11-30 16:26:58 +0300

    Подготовлена верстка для ДЗ

---
+++ lesson-4/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Галерея</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+  <link href="css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
+</head>
+
+<body>
+
+  <div class="photo-gallery">
+    <div class="container">
+      <div class="row photos">
+      
+        <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="images/desk.jpg" data-lightbox="photos"><img
+              class="img-fluid" src="images/desk.jpg"></a></div>
+        <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="images/building.jpg" data-lightbox="photos"><img
+              class="img-fluid" src="images/building.jpg"></a></div>
+        <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="images/loft.jpg" data-lightbox="photos"><img
+              class="img-fluid" src="images/loft.jpg"></a></div>
+        <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="images/build1.jpg" data-lightbox="photos"><img
+              class="img-fluid" src="images/build1.jpg"></a></div>
+        <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="images/diz1.jpg" data-lightbox="photos"><img
+              class="img-fluid" src="images/diz1.jpg"></a></div>
+        <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="images/desk.jpg" data-lightbox="photos"><img
+              class="img-fluid" src="images/desk.jpg"></a></div>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
+</body>
+
+</html>
